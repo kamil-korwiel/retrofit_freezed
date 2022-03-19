12a3595a70bc8143d865165113c743a79957fb46
kamil-korwiel <kamilkorwiel@gmail.com>
2022-03-19 12:23:37 +0100

    creating usecase for cardback

--- bin/retrofit_freezed.dart
+++ bin/retrofit_freezed.dart
@@ -3,20 +3,24 @@ import 'package:dio/dio.dart';
 import 'src/core/resources/data_state.dart';
 import 'src/data/remote/heartsone_api_service.dart';
 import 'src/data/repositories/heartsone_repository_impl.dart';
+import 'src/domain/usecase/get_card_backs_usecase.dart';
 
 void main(List<String> arguments) {
   final dio = Dio();
   final rest = HeartStoneApiService(dio);
   final repo = HeartsoneRepositoryImpl(rest);
+  final usecase = GetCardBacksUseCase(repo);
   
-  repo.getCardsBack().then((value) {
+  usecase().then((value) {
     if(value is DataSuccess){
       print(value.data!.length);
     }
     if(value is DataFailed){
-      print(value.error);
+      print(value.error!.message);
     }
     
   });
+
+
   
 }
